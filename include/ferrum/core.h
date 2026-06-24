a25859e0700caa7b682dab582235354ca9ef0f2e
K3130 <a07adf2d675c@mail.ru>
2026-06-24 11:16:57 +0300

    del b

--- include/ferrum/core.h
+++ include/ferrum/core.h
@@ -2,18 +2,6 @@
 #define FERRUM_CORE_H
 
 #define FERRUM_VERSION_MAJOR 0
-#define FERRUM_VERSION_MINOR 1
-#define FERRUM_VERSION_PATCH 0
-
-typedef enum 
-{
-	FR_OK            =  0,
-	FR_ERR           = -1,
-	FR_ERR_NOMEM     = -2,
-	FR_ERR_INVAL     = -3,
-	FR_ERR_TIMEOUT   = -4,
-	FR_ERR_CONN      = -5,
-} fr_result_t;
 
 
 
